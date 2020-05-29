328faa2b87cf5afa57e9ebd3b5c18941f7886b1c
EmiPiplu <62481444+EmiPiplu@users.noreply.github.com>
2020-05-29 14:12:21 +0100

    Add files via upload

--- js/Incremental.js
+++ js/Incremental.js
@@ -1,15 +1,34 @@
 var Number = 0;
+var HasLoaded = 0;
 
 function Increment() {
 	Number = Number + 1;
 	document.getElementById("Number").innerHTML = Number;
 }
 
-function Tick() {
+
+
+window.setInterval(function(){
+	
 	Number = Number + Incrementor1Qty + 2;
 	document.getElementById("Number").innerHTML = Number;
-}
-var Timer = window.setInterval(function(){Tick()}, 1000);
+	save() 
+	if (HasLoaded == 0) load()
+	
+}, 1000);
+
+
+window.onload = function() {
+  load();
+};
+
+
+
+
+
+
+
+
 
 var Incrementor1 = "Incrementor 1";
 var Incrementor1Cost = 20;
@@ -27,3 +46,27 @@ function BuyIncrementor1() {
 	}
 	
 }
+
+
+
+function save() {
+
+var save = {
+	Number: Number,
+	Incrementor1Cost: Incrementor1Cost,
+	Incrementor1Qty: Incrementor1Qty,
+}
+
+localStorage.setItem("save",JSON.stringify(save));
+
+}
+
+function load(){
+
+var savegame = JSON.parse(localStorage.getItem("save"));
+if (typeof savegame.Number !== "undefined") Number = savegame.Number;
+if (typeof savegame.Incrementor1Qty !== "undefined") Incrementor1Qty = savegame.Incrementor1Qty;
+document.getElementById("Number").innerHTML = Number
+document.getElementById("Incrementor1Qty").innerHTML = Incrementor1Qty;
+}
+
